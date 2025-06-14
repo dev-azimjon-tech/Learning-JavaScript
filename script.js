1364e704e698cbecea6c42b9ae27f38fa36e9ce3
Azimjon Sobirov <sobirovazimjon02@gmail.com>
2025-06-14 22:46:02 +0800

    Learned about variables and did some tasks

---
+++ script.js
@@ -0,0 +1,60 @@
+// There are 3 types of variables :
+//  var(old style),
+//  let(new,modern and recomended), 
+//  const(constant value can not be changed)
+
+
+// Variables lesson:
+// let name = "Azimjon";
+// let age = 16;
+// const country = "Tajikistan";
+
+// console.log("Your name is " + name + " you are " + age +" years old. And you are from " + country);
+
+// Tasks for variables
+// Task 1:
+// Create a variable for your school name and another for your city. Then imagine printing them together.
+
+// const school_name = "MTMU N5";
+// const city_name = "Gulakandoz";
+
+// console.log("My schools name is " + school_name + " and my city called " + city_name)
+
+// Task 2
+// Create a variable to store your favorite sport. Later, change it to a different sport.
+
+// let sport = "Football";
+// sport = "Basketball";
+
+// console.log("My favorite sport is " + sport)
+
+
+// Task 3
+// Think of two things you love doing. Create two variables for them, then create one more variable that combines both (like a sentence).
+
+// let sport = "Basketball";
+// let hobby = "Programming";
+// let sentence = sport + " and " + hobby;
+
+// console.log("I love " + sentence);
+
+// Task 4
+// Create two variables with different values. Then imagine you want to swap them — the first should now have the second’s value and vice versa.(Think how you’d do that using a third variable).
+
+// let a = 2;
+// let b = 10;
+// let c = a = b;
+// let d = b = a;
+
+// console.log("The switched  value  of  a: " + c + " The switched value of b: " + d);
+
+
+// Task 5
+
+// const name = "Azimjon";
+// let age = 16;
+// let favorite_subject = "English";
+// let future_job = "Programmer";
+
+// console.log("Hi, I'm " + name + " I'm " + age + " years old. My favorite subject is " + favorite_subject + " And my future job is " + future_job);
+
